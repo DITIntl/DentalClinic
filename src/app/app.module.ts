f981ec4e15f3509e0646a6b08f874afd43cc95e6
DESKTOP-L43R7OF\pawel <pawel.a.galas@gmail.com>
2017-11-16 23:04:13 +0100

    added login screen - just html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
 // components
 import { AppComponent } from './app.component';
 import { NavComponent } from './navigation/nav.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes =  [
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
