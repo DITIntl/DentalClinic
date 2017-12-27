4f4e1438879023af2868cb0dc6df52f785505cd3
PawelG-WWA <pawel.a.galas@gmail.com>
2017-12-27 23:17:29 +0100

    Feature/login screen (#1)
    * created navigation component
    
    * added login screen - just html
    
    * Added promo banner
    
    * modified angular cli json file
    
    * Login screen completed for desktop and iPhones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
+// core modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
+// components
 import { AppComponent } from './app.component';
+import { NavComponent } from './navigation/nav.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './pages/main/main.component';
+import { PromoBoxComponent } from './reusables/promo-box/promo-box.component';
+
+const routes: Routes =  [
+  { path: '', component: MainComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavComponent,
+    LoginComponent,
+    PromoBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
